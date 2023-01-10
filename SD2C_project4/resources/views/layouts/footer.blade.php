6f701b6b68ad0562381d1415c069914d345c76be
TutaiTran <ps234134@edu.summacollege.nl>
2023-01-10 23:12:45 +0100

    Home page bijna af, morgen beginnen met pizza page. about us in homepage nog bg veranderen

---
+++ SD2C_project4/resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-gray-900 text-white pt-8">
+    <div class="container mx-auto flex flex-wrap py-8">
+        <div class="w-full md:w-1/4 text-center px-4">
+            <a href="#" class="flex items-center justify-center mb-2">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Stonks</span>
+                <img src="{{asset('assets/img/logo.png')}}" class="h-9 mr-3 sm:h-9" alt="Logo" />
+            </a>
+            <p class="mb-2">There's no better feeling in the world than a warm pizza box on your lap ~Kevin James</p>
+        </div>
+        <div class="w-full md:w-1/4 text-center px-4">
+            <h2 class="font-medium text-lg mb-2">Populair</h2>
+            <ul class="list-none p-0">
+                <li class="mb-2">Pizza Salami</li>
+                <li class="mb-2">Pizza Hot&Spicy</li>
+                <li class="mb-2">Pizza Tonno</li>
+            </ul>
+        </div>
+        <div class="w-full md:w-1/4 text-center px-4">
+            <h2 class="font-medium text-lg mb-2">Vestigingen</h2>
+            <ul class="list-none p-0">
+                <li class="mb-2">Amsterdam</li>
+                <li class="mb-2">Eindhoven</li>
+                <li class="mb-2">Rotterdam</li>
+            </ul>
+        </div>
+        <div class="w-full md:w-1/4 text-center px-4">
+            <h2 class="font-medium text-lg mb-2">Overig</h2>
+            <ul class="list-none p-0">
+                <li class="mb-2">Algemene voorwaarden</li>
+                <li class="mb-2">Privacy statement</li>
+                <li class="mb-2">Cookie statement</li>
+            </ul>
+        </div>
+    </div>
+    <div class="bottom-0 bg-gray-800 py-4">
+        <div class="container mx-auto text-center text-sm">
+          &copy; 2023 All rights reserved. Made by <strong><a href="https://tutaitran.azurewebsites.net/" target="__blank">Tdesign </a></strong> Distributed By Azure websites
+        </div>
+      </div>
+</footer>
---
+++ SD2C_project4/resources/views/pizza/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app-layout')
+@section('content')
+    <div id="home" class="relative z-10 header-hero">
+        <div class="container">
+            <div class="justify-center row">
+                <div class="w-full lg:w-5/6 xl:w-2/3">
+                    <div class="pt-48 pb-64 header-content">
+                        <div class="flex items-center">
+                            <h3 class="mb-5 text-3xl font-semibold leading-tight text-gray-900 md:text-5xl">Welkom bij Stonks
+                            </h3>
+                            <img src="{{ asset('assets/img/logo.png') }}" class="h-9 mr-3 sm:h-32" alt="Logo" />
+                        </div>
+
+                        <p class="mb-10 text-xl text-gray-700">Elke dag vanaf 16:30 tot 01:00 open!<br>
+                            Bel: <a href="tel:+31642121289" class="underline">+31642121289</a> om een bestelling te maken of
+                            bestel via de webshop! </p>
+                        <ul class="flex flex-wrap">
+                            <li><button
+                                    class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                    <a
+                                        class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                        Bestel nu
+                                    </a>
+                                </button></li>
+                        </ul>
+                    </div> <!-- header content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- header content -->
+    <div class="container mx-auto py-8 bg-black ">
+        <div class="flex flex-wrap">
+            <div class="w-full md:w-1/2 aboutus">
+                <h1 class="text-4xl font-semibold mb-5">About Us</h1>
+                <p class="text-gray-700 text-justify text-lg">Stonks Pizza is een lokale pizzatent die gevestigd is in de stad en gekend is voor de heerlijke smaken en de verse ingrediënten die we gebruiken. Wij geloven in het maken van authentieke pizza's die in onze houtgestookte oven gebakken worden.
+                    Ons doel is om onze klanten de meest smakelijke pizza's aan te bieden die we kunnen maken. Wij zijn trots op ons team van professionele koks die ervaring hebben in de pizza-industrie en geobsedeerd zijn met het creëren van de perfecte pizza. Onze pizza's zijn gemaakt met de beste ingrediënten en altijd vers.
+                    Al onze vestigingen zijn gezellig ingericht met een rustieke en gezellige sfeer. Wij nodigen u uit om onze pizzas te komen proeven, onze klanten waarderen onze pizza's voor de smaak en de kwaliteit. Wij zijn verheugd om u te laten genieten van onze pizza's en u een geweldige tijd te bezorgen.</p>
+
+            </div>
+            <div class="w-full md:w-1/2 inline-flex items-center justify-center">
+                <img src="{{asset('assets/img/pizza_chef.jpg')}}" alt="Pizzas" class="w-4/6  object-cover rounded">
+            </div>
+        </div>
+    </div>
+@endsection
