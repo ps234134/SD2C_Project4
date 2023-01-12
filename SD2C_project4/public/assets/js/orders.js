5f18d071ab0c4da57cd701bc11adeb0649b7ffaa
TutaiTran <ps234134@edu.summacollege.nl>
2023-01-12 19:10:39 +0100

    menu responsive nog werken aan de popup als je op cart klikt

---
+++ SD2C_project4/public/assets/js/orders.js
@@ -0,0 +1,25 @@
+/* Order popup*/
+
+//  function showOrders() {
+//         // Create the pop-up screen
+//         var ordersPopup = document.createElement("div");
+//         ordersPopup.classList.add("orders-popup");
+
+//         // Add the orders list to the pop-up screen
+//         var ordersList = document.createElement("div");
+//         ordersList.classList.add("orders-list");
+//         ordersList.innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
+//         ordersPopup.appendChild(ordersList);
+
+//         // Add the close button to the pop-up screen
+//         var closeButton = document.createElement("button");
+//         closeButton.classList.add("close-button");
+//         closeButton.innerHTML = "X";
+//         closeButton.onclick = function() {
+//             document.body.removeChild(ordersPopup);
+//         }
+//         ordersPopup.appendChild(closeButton);
+
+//         // Append the pop-up screen to the body of the HTML document
+//         document.body.appendChild(ordersPopup);
+//     }
