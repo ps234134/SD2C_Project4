709f87d9dc71d7bc2854c88078fbbc51cc0e239a
TutaiTran <ps234134@edu.summacollege.nl>
2023-01-18 22:56:18 +0100

    test web

--- SD2C_project4/routes/web.php
+++ SD2C_project4/routes/web.php
@@ -39,5 +39,7 @@ Route::get('/contact', function() {
 Route::get('/status', function() {
     return view('pizza.status');
 });
-
+Route::post('/status', function(Request $request) {
+    return view('pizza.status');
+});
 Route::resource('pizza', PizzaController::class);
